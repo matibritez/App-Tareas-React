c2c98a2dd459e98edfffb3d2975587c47bdd8aa7
Matias Britez <89950148+matibritez@users.noreply.github.com>
2023-05-16 20:50:02 -0300

    crear-tareas

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import { useState } from 'react';
 import './App.css';
+import { TaskCreator } from './components/TaskCreator';
 
 function App() {
-
-  const [ newTaskName, setNewTaskName ] = useState();
-
-  const handleSubmit = (e) =>{
-    e.preventDefault();
-    localStorage.setItem('tasks', newTaskName)
-  }
-
   return (
     <div className="App">
-
-      <form onSubmit={handleSubmit}>
-        <input 
-           type='text'
-           placeholder='Enter a new Task'
-           onChange={(e)=> setNewTaskName(e.target.value)}
-         />
-        <button> Save Task</button>
-      </form>
+      <TaskCreator/>
     </div>
-
   );
 }
 
